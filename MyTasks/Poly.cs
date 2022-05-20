5064caf0e88a57093021df4eb036532cb4f27c2c
sknabirasool <sknabirasool104@gmail.com>
2022-05-20 16:45:35 +0530

    Add project files.

---
+++ MyTasks/Poly.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTasks
+{
+    public class Poly
+    {
+        // A few example members
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        // Virtual method
+        public virtual void Draw()
+        {
+            Console.WriteLine("Performing base class drawing tasks");
+        }
+    }
+
+    public class Circle : Poly
+    {
+        public override void Draw()
+        {
+            // Code to draw a circle...
+            Console.WriteLine("Drawing a circle");
+            base.Draw();
+        }
+    }
+    public class Rectangle : Poly
+    {
+        public override void Draw()
+        {
+            // Code to draw a rectangle...
+            Console.WriteLine("Drawing a rectangle");
+            base.Draw();
+        }
+    }
+    public class Triangle : Poly
+    {
+        public override void Draw()
+        {
+            // Code to draw a triangle...
+            Console.WriteLine("Drawing a triangle");
+            base.Draw();
+        }
+    }
+
+}
---
+++ MyTasks/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+namespace MyTasks;
+class Program
+{
+    static void Main(String[] args)
+    {
+        var account = new BankAccount("ICICI BANK", 100000);
+        Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.balance}");
+        account.MakeWithdrawal(150, DateTime.Now, "Restarent");
+        Console.WriteLine(account.balance);
+
+        Console.WriteLine(account.GetAccountHistory());
+        // Test that the initial balances must be positive.
+       
+        try
+        {
+            var invalidAccount = new BankAccount("invalid", -55);
+        }
+        catch (ArgumentOutOfRangeException e)
+        {
+            Console.WriteLine("Exception caught creating account with negative balance");
+            Console.WriteLine(e.ToString());
+            return;
+        }
+        account.MakeDeposit(12000, DateTime.Now, "Transfer To Account Holder");
+        Console.WriteLine(account.balance);
+
+        
+    }
+}
+
